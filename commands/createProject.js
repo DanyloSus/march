bb30a584667fa53a60e76aff43d280083f08e003
Danylo Sushko <dsu@empat.tech>
2024-12-25 19:13:01 +0200

    feat: add rewriting name of the project on package.json and installing project npm dependencies

--- commands/createProject.js
+++ commands/createProject.js
@@ -1,5 +1,7 @@
 import chalk from "chalk";
 import { exec } from "child_process";
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
 
 const repoHTTPS =
   "https://dsu1@bitbucket.org/empat_tech/react_modular_architecture_template.git";
@@ -19,9 +21,23 @@ export function cloneRepo(projectName) {
     console.log(chalk.green(stdout));
     console.log(chalk.green(`Repository cloned to ./${projectName}`));
 
-    // Initialize a new Git repository
+    // Update package.json
+    const packageJsonPath = join(process.cwd(), projectName, "package.json");
+    const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
+    packageJson.name = projectName;
+    writeFileSync(
+      packageJsonPath,
+      JSON.stringify(packageJson, null, 2),
+      "utf8"
+    );
+    console.log(chalk.green(`Updated package.json name to ${projectName}`));
+
+    console.log(
+      chalk.blue("Download dependencies and initialize a new Git repository...")
+    );
+    // Initialize a new Git repository and install dependencies
     exec(
-      `cd ./${projectName} && rm -rf .git && git init`,
+      `cd ./${projectName} && rm -rf .git && git init && npm install`,
       (initError, initStdout, initStderr) => {
         if (initError) {
           console.error(
@@ -36,7 +52,9 @@ export function cloneRepo(projectName) {
         }
         console.log(chalk.green(initStdout));
         console.log(
-          chalk.green(`Initialized a new Git repository in ./${projectName}`)
+          chalk.green(
+            `Initialized a new Git repository and installed dependencies in ./${projectName}`
+          )
         );
       }
     );
