6673278fe9442a9d9c776369203f0c7442ba8eba
Danylo Sushko <dsu@empat.tech>
2024-12-26 21:02:57 +0200

    feat: add capitalizing name of components and its modules

--- commands/createComponent.js
+++ commands/createComponent.js
@@ -1,14 +1,30 @@
 import { dirname, join, resolve } from "path";
 import { createDirectoryIfNotExists, writeFile } from "../helpers/index.js";
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function capitalizeComponentName(componentName) {
+  return componentName.split("/").map(capitalizeFirstLetter).join("/");
+}
+
 export function createComponent(componentName, options) {
-  const moduleName = options.module;
+  const moduleName = capitalizeComponentName(options.module);
+  const capitalizedComponentName = capitalizeComponentName(componentName);
   const baseDir = moduleName
     ? resolve(process.cwd(), "src/modules", moduleName, "components")
     : resolve(process.cwd(), "src/components");
-  const componentFolderPath = join(baseDir, dirname(`${componentName}/index`));
-  const componentFilePath = join(baseDir, componentName, "index.tsx");
-  const stylesFilePath = join(baseDir, componentName, "styles.ts");
+  const componentFolderPath = join(
+    baseDir,
+    dirname(`${capitalizedComponentName}/index`)
+  );
+  const componentFilePath = join(
+    baseDir,
+    capitalizedComponentName,
+    "index.tsx"
+  );
+  const stylesFilePath = join(baseDir, capitalizedComponentName, "styles.ts");
 
   // Create necessary directories
   createDirectoryIfNotExists(baseDir);
@@ -22,13 +38,13 @@ import { Box } from "ui/Box";
 
 import { styles } from "./styles";
 
-interface ${componentName.split("/").pop()}Props {
+interface ${capitalizedComponentName.split("/").pop()}Props {
     // Add your props here
 }
 
-export const ${componentName.split("/").pop()}: FC<${componentName
+export const ${capitalizedComponentName
     .split("/")
-    .pop()}Props> = () => {
+    .pop()}: FC<${capitalizedComponentName.split("/").pop()}Props> = () => {
     return (
         <Box sx={styles.root}>
           Dummy component
